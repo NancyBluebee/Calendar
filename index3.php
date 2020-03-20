417440df26160f05a766c6a1d642f1e73bc6a49f
nancychen <beekingdom0201@gmail.com>
2020-03-20 16:14:30 +0800

    nancy

---
+++ index3.php
@@ -0,0 +1,19 @@
+<?php
+
+//宣告一個變數
+$a=1;
+echo $a;
+echo "<br>";
+$a=2.1;
+echo $a;
+echo "<br>";
+print($a);
+echo "<br>";
+$studentInSchool="yes";
+
+
+define("PI",3.1415927);
+echo PI;
+echo PI*2*10;
+
+?>
