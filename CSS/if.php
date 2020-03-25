1c69435dba6ded44fcaad02eb270c2b44a305054
nancychen <beekingdom0201@gmail.com>
2020-03-25 14:52:59 +0800

    20200325修正

---
+++ CSS/if.php
@@ -0,0 +1,11 @@
+<?php
+//if 判斷式
+
+$score=70;
+
+if($score>=60){
+    echo "及格";
+}else{
+    echo "不及格";
+}
+
--- index.php
+++ index.php
@@ -13,3 +13,25 @@ echo $b++;
 echo "<hr>";
 echo $b
 
+
+$a="A";
+$b=1;
+echo $a;
+echo "<br>"
+$c=$a.$b;
+$d=$a+$b;
+echo "hr";
+echo $c;
+echo "<hr>";
+echo d;
+
+//三元運算子
+$a=0;
+
+echo "<hr>"
+
+if($a>0){
+    echo "很貴";
+}else{
+    echo "免費"
+}
