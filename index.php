6cdd907f997fbc0d7151b65a5a8a819a397eca28
Nancy Chen <62413571+NancyBluebee@users.noreply.github.com>
2020-04-17 22:16:39 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,14 @@
+<html>
+<head>   
+<link href="calendar.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
 <?php
-
-$a=true;
-echo $a;
-echo "hr";
-echo !a;
-
-
-//運算子++ ==>$b=$b+1
-echo "<hr>";
-$b=1;
-echo $b++;
-echo "<hr>";
-echo $b
-
-
-$a="A";
-$b=1;
-echo $a;
-echo "<br>"
-$c=$a.$b;
-$d=$a+$b;
-echo "hr";
-echo $c;
-echo "<hr>";
-echo d;
-
-//三元運算子
-$a=0;
-
-echo "<hr>"
-
-if($a>0){
-    echo "很貴";
-}else{
-    echo "免費"
-}
+include 'calendar.php';
+ 
+$calendar = new Calendar();
+ 
+echo $calendar->show();
+?>
+</body>
+</html>  
